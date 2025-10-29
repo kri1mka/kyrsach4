8db258fa5c2721972e5069acdbe70d67a250c554
krist <kristinarekes@gmail.com>
2025-10-29 12:33:53 +0300

    Gradle settings changed 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     defaultConfig {
         applicationId = "com.example.kyrsach4"
         minSdk = 25
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
