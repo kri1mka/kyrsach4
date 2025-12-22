736c20962cd1a3292320130484b58297c9ee20c1
DIANA <stramokdiana.itpa@gmail.com>
2025-12-22 14:55:16 +0300

    edit files

--- app/src/main/java/com/example/kyrsach4/entities/UserInfo.java
+++ app/src/main/java/com/example/kyrsach4/entities/UserInfo.java
@@ -7,6 +7,7 @@ public class UserInfo {
     private int userId;
     private String sex;
     private int age;
+    private String avatarUrl;
 
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
@@ -19,5 +20,8 @@ public class UserInfo {
 
     public int getAge() { return age; }
     public void setAge(int age) { this.age = age; }
+
+    public String getAvatarUrl() { return avatarUrl; }
+    public void setAvatarUrl(String avatarUrl) { this.avatarUrl = avatarUrl; }
 }
 
